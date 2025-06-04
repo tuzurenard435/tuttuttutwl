1cbc1ee3ae4631372053a282bf744694ea40d165
tuzurenard435 <henrik.grueter@gmail.com>
2025-06-04 17:59:39 +0200

    Update usercolors.lua

--- usercolors.lua
+++ usercolors.lua
@@ -1,5 +1,12 @@
 return {
+  --indieuns_fan
   [2412582721] = Color3.fromRGB(0, 0, 0),
+  --sjsni20
   [4751887553] = Color3.fromRGB(0, 0, 0),
+  --gta5yyd
   [2656352954] = Color3.fromRGB(0, 0, 0),
+  --Dino
+  [3634405450] = Color3.fromRGB(141, 0, 255),
+  --Artist
+  [3678793516] = Color3.fromRGB(65, 35, 186),
 }
