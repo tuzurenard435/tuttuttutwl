ca72c95be5ea13b68f328cb09850643a16fa4605
tuzurenard435 <henrik.grueter@gmail.com>
2025-06-04 17:57:05 +0200

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,8 +1,13 @@
 return {
-  [3634405450] = true,
+  --Dino
+  [3634405450] = true, 
+  --Artist
   [3678793516] = true,
+  --indieuns_fan
   [2412582721] = true,
+  --sjsni20
   [4751887553] = true,
+  --gta5yyd
   [2656352954] = true,
 }
 
